5693f759cca7c4639221a33374f7863725d6d7b1
Sunny Tseng <tzuchientseng@gmail.com>
2024-09-05 05:45:30 +0800

    Update

--- lesson_2/240903/app.py
+++ lesson_2/240903/app.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 
 url = 'https://tw-nba.udn.com/nba/index'
 resp = requests.get(url)
-# resp = requests.get(url) 
 # print(resp.text) # 200
 
 soup = BeautifulSoup(resp.text, 'html.parser')
 # print(soup)
 # print(soup.prettify())
 
-geth1 = soup.find('h1').text # 不含標籤
-print(geth1)
+# geth1 = soup.find('h1').text # 不含標籤
+geth1_list = soup.find_all('h1') 
+for h1 in geth1_list:
+    print(h1.text)  # 輸出每個 <h1> 標籤中的文字
 
 getp = soup.find('p').text
 print(getp)

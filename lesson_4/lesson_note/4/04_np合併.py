661435f628f477cb167d3ad1c47017b6721f1958
Sunny Tseng <tzuchientseng@gmail.com>
2024-12-09 22:37:33 +0800

    Python Lesson: statistics

--- lesson_4/lesson_note/4/04_np合併.py
+++ lesson_4/lesson_note/4/04_np合併.py
@@ -3,7 +3,16 @@ a = [1,2,3,4]
 b = [5,6,7,8]
 c = np.c_[a, b] # 合併column
 print(c)
-d = np.r_[a, b] # 陣列
+"""
+[[1 5]
+ [2 6]
+ [3 7]
+ [4 8]]
+"""
+d = np.r_[a, b] # numpy陣列
 print(d)
-print(a + b)#list
-print(np.array(a + b)) # 陣列
+# [1 2 3 4 5 6 7 8]
+print(a + b) # list
+# [1, 2, 3, 4, 5, 6, 7, 8]
+print(np.array(a + b)) # numpy陣列
+# [1 2 3 4 5 6 7 8]

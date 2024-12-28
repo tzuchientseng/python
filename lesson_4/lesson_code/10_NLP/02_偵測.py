e69e51490dd753d918a7f935b02832f2d3fb4e6f
Sunny Tseng <tzuchientseng@gmail.com>
2024-12-28 12:24:33 +0800

    update

---
+++ lesson_4/lesson_code/10_NLP/02_偵測.py
@@ -0,0 +1,15 @@
+from gensim.models import Word2Vec
+model=Word2Vec.load("w2v_model")
+rs=model.wv.most_similar("orange".lower())
+for r in rs:
+    print(r)
+'''
+#colab使用如下
+from google.colab import drive
+from gensim.models import Word2Vec
+drive.mount('/data')
+model= Word2Vec.load("/data/MyDrive/w2v_model")
+rs=model.wv.most_similar("Computer".lower())
+for r in rs:
+    print(r)
+'''
